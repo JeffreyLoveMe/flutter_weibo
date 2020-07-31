f48c2dd738abebf2f8ad99674ea4425f4e87fdf8
xiewujun <xiewujun@thedream.cc>
2020-07-31 17:59:49 +0800

    完善底部分栏

--- flutter_weibo/lib/pages/comment/index_page.dart
+++ flutter_weibo/lib/pages/comment/index_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_weibo/pages/Profile/profile_page.dart';
 import 'package:flutter_weibo/pages/discover/discover_page.dart';
 import 'package:flutter_weibo/pages/home/home_page.dart';
@@ -13,41 +14,76 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
-  // 分栏 - icon必须加上后缀
-  List<BottomNavigationBarItem> bottomNavigationBarItems = [
-    BottomNavigationBarItem(
-        icon: Image.asset('assets/images/weibo_login_qq.png',
-            width: 25.0, height: 25.0),
-        title: Text('首页')),
-    BottomNavigationBarItem(
-        icon: Image.asset('assets/images/weibo_login_qq.png',
-            width: 25.0, height: 25.0),
-        title: Text('视频')),
-    BottomNavigationBarItem(
-        icon: Image.asset('assets/images/weibo_login_qq.png',
-            width: 25.0, height: 25.0),
-        title: Text('发现')),
-    BottomNavigationBarItem(
-        icon: Image.asset('assets/images/weibo_login_qq.png',
-            width: 25.0, height: 25.0),
-        title: Text('消息')),
-    BottomNavigationBarItem(
-        icon: Image.asset('assets/images/weibo_login_qq.png',
-            width: 25.0, height: 25.0),
-        title: Text('我')),
-  ];
   // 当前选中index
   int _currentSelectedIndex = 0;
 
+  List<List<String>> tabIcons = [
+    [
+      'assets/images/tabbar_home.png',
+      'assets/images/tabbar_home_highlighted.png'
+    ],
+    [
+      'assets/images/tabbar_video.png',
+      'assets/images/tabbar_video_highlighted.png'
+    ],
+    [
+      'assets/images/tabbar_discover.png',
+      'assets/images/tabbar_discover_highlighted.png'
+    ],
+    [
+      'assets/images/tabbar_message_center.png',
+      'assets/images/tabbar_message_center_highlighted.png'
+    ],
+    [
+      'assets/images/tabbar_profile.png',
+      'assets/images/tabbar_profile_highlighted.png'
+    ],
+  ];
+
+  List<String> tabTitles = ['首页', '视频', '发现', '消息', '我'];
+
+  var lastExitTime;
+
+  Widget getTabIcon(int index) {
+    if (index == _currentSelectedIndex) {
+      // 选中
+      return Image.asset(tabIcons[index][1], width: 25.0, height: 25.0);
+    } else {
+      // 未选中
+      return Image.asset(tabIcons[index][0], width: 25.0, height: 25.0);
+    }
+  }
+
+  Widget getTabTitle(int index) {
+    if (index == _currentSelectedIndex) {
+      // 选中
+      return Text(tabTitles[index],
+          style: TextStyle(fontSize: 13.0, color: Colors.black));
+    } else {
+      // 未选中
+      return Text(tabTitles[index],
+          style: TextStyle(fontSize: 13.0, color: Colors.black));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    // 分栏 - icon必须加上后缀
+    // ！！！构造函数只能调用static方法 - https://www.codeleading.com/article/32302429718
+    List<BottomNavigationBarItem> bottomNavigationBarItems = [
+      BottomNavigationBarItem(icon: getTabIcon(0), title: getTabTitle(0)),
+      BottomNavigationBarItem(icon: getTabIcon(1), title: getTabTitle(1)),
+      BottomNavigationBarItem(icon: getTabIcon(2), title: getTabTitle(2)),
+      BottomNavigationBarItem(icon: getTabIcon(3), title: getTabTitle(3)),
+      BottomNavigationBarItem(icon: getTabIcon(4), title: getTabTitle(4)),
+    ];
     // ???
     return SafeArea(
         child: WillPopScope(
             child: Scaffold(
               bottomNavigationBar: BottomNavigationBar(
                 // 背景颜色
-                backgroundColor: Colors.grey,
+                backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
                 // 当前有多少Page
                 items: bottomNavigationBarItems,
                 // 类型
@@ -56,7 +92,11 @@ class _IndexPageState extends State<IndexPage> {
                 currentIndex: _currentSelectedIndex,
                 // 点击Item
                 onTap: (index) {
-                  // FIMXE - 点击Item
+                  // 点击Item
+                  // ???
+                  setState(() {
+                    _currentSelectedIndex = index;
+                  });
                 },
               ),
               // IndexedStack???
@@ -71,7 +111,22 @@ class _IndexPageState extends State<IndexPage> {
                 ],
               ),
             ),
-            // ???
-            onWillPop: null));
+            // WillPopScope组件 - 双击退出App组件
+            // Future<bool> Function() - 返回值Future<bool>/无参
+            // void Function(int) - 参数是int/无返回值
+            onWillPop: () async {
+              if (lastExitTime == null ||
+                  DateTime.now().difference(lastExitTime) >
+                      Duration(seconds: 2)) {
+                lastExitTime = DateTime.now();
+                // toast
+                return false;
+              } else {
+                lastExitTime = DateTime.now();
+                // 退出App
+                SystemChannels.platform.invokeListMethod('SystemNavigator.pop');
+                return true;
+              }
+            }));
   }
 }
